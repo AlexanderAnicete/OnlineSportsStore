a2edd3e091b7b2e2f270998805cbe8c09b8ebd5a
Alexander Anicete <alexander.anicete91@gmail.com>
2019-03-25 09:41:36 -0700

    Updated

---
+++ SportsStore.Database/Script1.sql
@@ -0,0 +1,13 @@
+﻿INSERT INTO Products
+(Product.Name, Product.Description, Product.Category, Product.Price)
+VALUES
+('Peanut Butter', 'Butter made of peanuts', 'condiments', 3),
+('Chocolate', 'Delicious', 'candy', 5),
+('Game', 'Video Game', 'Games', 60),
+('Ring', 'wedding band', 'jewelery', 5000),
+('Alienware', 'gaming laptop', 'Electonics', 2000),
+('Chair', 'Table Chair', 'Furniture', 50),
+('Razor Mouse', 'Gaming Mouse', 'Electronics', 100),
+('Corsair K70', 'Gaming Keyboard', 'Electronics', 150),
+('Trojan', 'Protection', 'Baby Blocker', 5),
+('Ciroc', 'Liquor', 'Alcohol', 10);
